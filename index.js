4745da73550f2fbb3a5766c68419b73332a51bf0
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-04-27 11:03:38 -0300

    importing socket.io

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const socketIO = require('socket.io');
 
 const list = ['Banana', 'Apple', 'Mango'];
 
@@ -10,6 +11,19 @@ app.get('/list', (req, res) =>{
 
 app.use('/', express.static(path.join(__dirname, '/public')));
 
-app.listen(3000, () =>{
+const server = app.listen(3000, () =>{
     console.log('App running!');
+});
+
+
+const io = socketIO(server) // server é quem vai estar gerenciando as reqs http
+
+/*  io se refere a input/output, e funciona como um "listener" para quando ocorrer um evento, neste caso "connection".
+    Esta função socket é a conexão entre back e front, cada browser se conectando será uma representação do socket
+    Precisamos preparar o front para receber essa conexão (importar no front)
+*/
+io.on('connection', (socket)=>{
+    console.log('New connection');
 })
+
+
