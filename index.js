4282d3bfa756ba23ff2336c3318469b9b8484dea
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-04-27 15:15:33 -0300

    emitindo "mesmo numero de série" para todos

--- index.js
+++ index.js
@@ -25,9 +25,15 @@ const io = socketIO(server) // server é quem vai estar gerenciando as reqs http
 io.on('connection', (socket)=>{
     console.log('New connection');
 
-    // emitindo para o socket.on() - os argumentos devem dar match
-    socket.emit('hello', {msg: "Welcome from back"})
+    const randomNum = Math.random();
+    //Math.round(Math.random() * 10)
 
+    // emitindo para o socket.on() - os argumentos devem dar match
+    //socket.emit('hello', {msg: `Welcome from back! ${randomNum}`})
+
+    //emitindo para todos user uma emissão com o mesmo numero basta trocar para io.
+    io.emit('hello', {msg: `Welcome from back! ${randomNum}`})
+    
     socket.on('hello_from_front', (data) =>{
         console.log(data.msg);
     })
